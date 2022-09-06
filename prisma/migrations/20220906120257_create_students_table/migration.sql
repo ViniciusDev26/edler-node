c8cf8139913e576c8edbaa573af25e4f8bf7fc33
Carlos Vinicius <viniciusdev.26@gmail.com>
2022-09-06 12:24:03 -0300

    chore: change mysql database to postgresql

---
+++ prisma/migrations/20220906120257_create_students_table/migration.sql
@@ -0,0 +1,8 @@
+-- CreateTable
+CREATE TABLE "students" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "birthDate" BIGINT NOT NULL,
+
+    CONSTRAINT "students_pkey" PRIMARY KEY ("id")
+);
